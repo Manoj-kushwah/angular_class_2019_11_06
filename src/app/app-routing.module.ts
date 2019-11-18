7df16c7612d536646bf00975361d2749d739c88f
design_jsi <design@jsinformatics.com>
2019-11-18 14:30:30 +0530

    Login, About, Contact modules implemented.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './home/home.module#HomeModule'}
+  {path: '', loadChildren: './home/home.module#HomeModule'},
+  {path: 'login', loadChildren: './login/login.module#LoginModule'},
+  {path: 'contact', loadChildren: './contact/contact.module#ContactModule'},
+  {path: 'about', loadChildren: './about/about.module#AboutModule'}
 ];
 
 @NgModule({
