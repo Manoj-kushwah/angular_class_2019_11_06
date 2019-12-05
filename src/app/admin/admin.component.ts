403d8d7f07589fab27de4162ab08d928b2758b0d
design_jsi <design@jsinformatics.com>
2019-12-05 14:06:56 +0530

    Get user api implemented.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+import { Router } from '@angular/router';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-admin',
@@ -8,9 +10,20 @@ import { AuthService } from '../shared/auth.service';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private api: ApiService, private route: Router) { }
 
   ngOnInit() {
+    if (!this.auth.isLogin()) {
+      this.route.navigate(["","login"]).then(res=>{
+      }).catch(err=>{
+      });
+    }
+
+    this.api.getUsers().then(res=>{
+      console.log("AdminComponent: res ", res);
+    }).catch(err=>{
+      
+    });
   }
 
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -13,6 +13,7 @@ export class ApiService {
   public login(email: string, password: string): Promise<any> {
     let url: string = "https://jsistudentportal.000webhostapp.com/api/user/login/";
     let query: string = "?email=".concat(email, "&password=", password);
+    console.log("ApiService: query ", query);
     console.log("ApiService: url ", url + query);
     return fetch(url + query).then(res=> {
       console.log("ApiService: res", res);
@@ -22,4 +23,26 @@ export class ApiService {
       return err;
     })
   }
+
+  /**
+   * getUsers
+   */
+  public getUsers(): Promise<any> {
+    let url: string = "https://jsistudentportal.000webhostapp.com/api/user/get/";
+    return fetch(url).then(res=>{
+      console.log("ApiService: url ", url);
+      return res.json();
+    }).catch(err=>{
+      return err;
+    })
+  }
 }
+
+/**
+function myFun(p1, p2){
+  return null;
+}
+const myFun = (p1, p2) => {
+  return null;
+}
+ */
