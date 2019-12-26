cc322e058c0a41c51b083fafc48c0bb19ad01a22
umashankar <umapatidar28@gmail.com>
2019-12-26 14:26:27 +0530

    Add new user form implemented.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AdminComponent],
   imports: [
     CommonModule,
+    FormsModule,
     AdminRoutingModule
   ]
 })
